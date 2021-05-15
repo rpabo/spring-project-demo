5c1d2eba5191668861914ee4ebc678e103b719bf
student <robert.pabo@ngc.com>
2021-05-15 16:58:54 +0000

    Initial commit

---
+++ src/test/java/com/rpabo/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpabo.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
